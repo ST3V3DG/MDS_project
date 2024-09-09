b885002ce338ac12d8c9affd9d7a9ec8e2edd460
ST3V3DG <168684746+ST3V3DG@users.noreply.github.com>
2024-09-09 16:36:18 +0100

    Update /projects/ urls.py

--- personnel/urls.py
+++ personnel/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     path('activate/<uidb64>/<token>', views.activate,name='activate'),
     path("candidature/<str:username>/", views.candidature,name='candidature'),
     path("modifier/<str:username>/", views.update, name='update'),
+    path("projets/<str:username>/", views.projets, name='projets'),
     
 ]
 if settings.DEBUG:
